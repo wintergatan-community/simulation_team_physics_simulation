3e113bce3bdfafd532f2d2fc66f3b469d4dcad2b
IdrilAndariel <margareth.warburton@gmail.com>
2021-09-09 17:14:41 +0200

    Replace cubes with spheres, create new cubes

--- Source/Test/HelloWorldAddon/MoveObjectXY.py
+++ Source/Test/HelloWorldAddon/MoveObjectXY.py
@@ -39,10 +39,35 @@ class ObjectMoveXY(bpy.types.Operator):
             obj.location.x += -1.0
             obj.location.y += -2.0
             obj.location.z += 0.5
+            print(obj.name)
             print(os.path.realpath(__file__)) # this shows that the file *seems* to be saved at another location, that's why the relative paths didn't work 
             hw.print_hello_world() # this shows whether the import of a simple file works
-        
-        
+            
+            if obj.name == 'Cube':
+                
+                marble_x = obj.location.x
+                marble_y = obj.location.y
+                marble_z = obj.location.z
+                bpy.data.objects['Cube'].select_set(True)
+                bpy.ops.object.delete()
+                bpy.ops.surface.primitive_nurbs_surface_sphere_add(radius=1, enter_editmode=False, align='WORLD', location=(marble_x, marble_y, marble_z), scale=(1, 1, 1))
+                
+                new_cube_1_x = marble_x + 2.0
+                new_cube_1_y = marble_y + 2.0
+                new_cube_1_z = marble_z + 2.0
+                
+                new_cube_2_x = marble_x - 2.0
+                new_cube_2_y = marble_y - 2.0
+                new_cube_2_z = marble_z - 2.0
+                
+                try:
+                    bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(new_cube_1_x, new_cube_1_y, new_cube_1_z), scale=(1, 1, 1))
+                except:
+                    print('space for cube 1 already occupied')
+                try:
+                    bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(new_cube_2_x, new_cube_2_y, new_cube_2_z), scale=(1, 1, 1))
+                except:
+                    print('space for cube 2 already occupied')
 
         return {'FINISHED'}            # Lets Blender know the operator finished successfully.
 
